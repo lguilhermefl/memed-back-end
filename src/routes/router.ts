5858b8d96ef84187cc6dc2ae3f90d2f1620d669e
luiz <lguilhermefl@gmail.com>
2022-09-30 20:32:14 -0300

    feat: add test file router

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,5 +1,7 @@
 import { Router } from "express";
+import testFilesRouter from "./testFilesRouter";
 
 const router = Router();
+router.use("/test-files", testFilesRouter);
 
 export default router;
