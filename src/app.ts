e86b5a5ea8db072661b8daaa77e43e9aac650f7e
luiz <lguilhermefl@gmail.com>
2022-09-30 19:41:54 -0300

    chore: add urlencoded

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,11 @@
-import express, { json } from "express";
+import express, { json, urlencoded } from "express";
 import "express-async-errors";
 import router from "./routes/router";
 import errorHandlerMiddleware from "./middlewares/errorMiddleware";
 
 const app = express();
 app.use(json());
+app.use(urlencoded({ extended: true }));
 
 app.use(router);
 app.use(errorHandlerMiddleware);
