fb243e35f756446e923994002521fe01a0d6ea1e
luiz <lguilhermefl@gmail.com>
2022-09-30 20:45:55 -0300

    refactor: update fileTest types and imports

--- src/services/testFilesService.ts
+++ src/services/testFilesService.ts
@@ -1,10 +1,10 @@
 import s3Config from "../config/s3";
 import { DeleteObjectCommand } from "@aws-sdk/client-s3";
-import { TCreateTest } from "../types/fileType";
+import { TCreateTestFile } from "../types/fileTestType";
 import * as testFilesRepository from "../repositories/testFilesRepository";
 import { notFoundError, conflictError } from "../utils/errorUtils";
 
-export async function insert(file: TCreateTest) {
+export async function insert(file: TCreateTestFile) {
   const fileWithKey = await testFilesRepository.findByKey(file.key);
   const fileWithUrl = await testFilesRepository.findByUrl(file.url);
 
