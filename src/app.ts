4b7932b49e0233933db2553174abce1216460fe0
luiz <lguilhermefl@gmail.com>
2022-09-30 19:42:20 -0300

    chore: add cors

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express, { json, urlencoded } from "express";
+import cors from "cors";
 import "express-async-errors";
 import router from "./routes/router";
 import errorHandlerMiddleware from "./middlewares/errorMiddleware";
 
 const app = express();
 app.use(json());
+app.use(cors());
 app.use(urlencoded({ extended: true }));
 
 app.use(router);
