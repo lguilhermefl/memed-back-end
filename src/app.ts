27f7997ab628cea39642f4b1525d627a6ef9acca
luiz <lguilhermefl@gmail.com>
2022-09-30 19:41:03 -0300

    feat: add error handler middleware

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import express, { json } from "express";
 import "express-async-errors";
 import router from "./routes/router";
+import errorHandlerMiddleware from "./middlewares/errorMiddleware";
 
 const app = express();
 app.use(json());
+
 app.use(router);
+app.use(errorHandlerMiddleware);
 
 export default app;
