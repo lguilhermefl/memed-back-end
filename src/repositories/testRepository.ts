a9eae416a6db443f9e8dddbde32ea3331b4deb4b
luiz <lguilhermefl@gmail.com>
2022-10-03 16:01:08 -0300

    feat: add find all by user id with files info

--- src/repositories/testRepository.ts
+++ src/repositories/testRepository.ts
@@ -1,7 +1,6 @@
 import { prisma } from "../config/database";
-import { TCreateTest } from "../types/testType";
 
-export async function insert(test: TCreateTest) {
+export async function insert(test: any) {
   return await prisma.tests.create({
     data: test,
   });
@@ -11,4 +10,19 @@ export async function findById(id: number) {
   return await prisma.tests.findUnique({
     where: { id },
   });
+}
+
+export async function findAllByUserIdWithFiles(userId: number) {
+  return await prisma.$queryRaw`
+    select t.id, t.title, t.notes, t.date, array(
+      select coalesce(
+        json_build_object('id', tf.id, 'name', tf.name, 'size', tf.size, 'url', tf.url)
+      , '[] '
+      ) from "testsFiles" tf
+      where tf."testId"=t.id
+    ) as files
+    from tests t
+    where t."userId"=${userId}
+    order by t.date
+  `;
 }
