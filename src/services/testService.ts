a1828675b75cc11efc4303d63b4dbb67d2116fc4
luiz <lguilhermefl@gmail.com>
2022-10-03 16:11:50 -0300

    feat: add get user tests with files

--- src/services/testService.ts
+++ src/services/testService.ts
@@ -4,3 +4,7 @@ import * as testRepository from "../repositories/testRepository";
 export async function insert(test: TCreateTest) {
   return await testRepository.insert(test);
 }
+
+export async function getAllByUserIdWithFiles(userId: number) {
+  return await testRepository.findAllByUserIdWithFiles(userId);
+}
