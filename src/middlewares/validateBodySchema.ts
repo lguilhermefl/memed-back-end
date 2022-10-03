a380a8b965fbfa9284caa0789dbe55ff85857594
luiz <lguilhermefl@gmail.com>
2022-10-03 15:53:26 -0300

    feat: add validate body schema middleware

---
+++ src/middlewares/validateBodySchema.ts
@@ -0,0 +1,15 @@
+import { ObjectSchema } from "joi";
+import { NextFunction, Request, Response } from "express";
+
+export default function validateBodySchemaMiddleware(schema: ObjectSchema) {
+  return (req: Request, res: Response, next: NextFunction) => {
+    const { error } = schema.validate(req.body, { abortEarly: false });
+    if (error) {
+      return res
+        .status(422)
+        .send(error.details.map((detail: any) => detail.message));
+    }
+
+    next();
+  };
+}
