c7cb79ac04498589b3af35c1f192d221d66d9648
luiz <lguilhermefl@gmail.com>
2022-10-03 16:20:15 -0300

    feat: add sign in route

--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -4,5 +4,6 @@ import * as userController from "../controllers/userController";
 const userRouter = Router();
 
 userRouter.post("/sign-up", userController.insert);
+userRouter.post("/", userController.signIn);
 
 export default userRouter;
