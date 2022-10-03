95dfc4499782c5bbb4d79f4dd4ed30c45e4e9790
luiz <lguilhermefl@gmail.com>
2022-10-03 15:55:03 -0300

    chore: add test types

--- src/types/testType.ts
+++ src/types/testType.ts
@@ -2,3 +2,5 @@ import { Tests } from "@prisma/client";
 
 export type TTest = Tests;
 export type TCreateTest = Omit<TTest, "id" | "createdAt">;
+export type TTestData = Omit<TTest, "id" | "userId" | "createdAt">;
+export type TUpdateTest = Partial<TTest>;
