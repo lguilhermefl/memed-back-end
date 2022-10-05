00f3af23643d3b7f24259e5aee14cfcae66aabcf
luiz <lguilhermefl@gmail.com>
2022-10-05 20:20:56 -0300

    feat: add validate test schema

--- src/routes/testRouter.ts
+++ src/routes/testRouter.ts
@@ -1,11 +1,17 @@
 import { Router } from "express";
 import * as testController from "../controllers/testController";
 import validateJWT from "../middlewares/validateJwtMiddleware";
+import { testSchema } from "../schemas/testSchema";
+import validateBodySchemaMiddleware from "../middlewares/validateBodySchema";
 
 const testRouter = Router();
 
 testRouter.use(validateJWT());
-testRouter.post("/", testController.insert);
+testRouter.post(
+  "/",
+  validateBodySchemaMiddleware(testSchema),
+  testController.insert
+);
 testRouter.get("/", testController.getAll);
 testRouter.delete("/", testController.remove);
 
