24b8236df97ef91f3567daf03912350247c821e7
luiz <lguilhermefl@gmail.com>
2022-10-05 16:35:44 -0300

    fix: correct parameter type

--- src/utils/removeMultipleFiles.ts
+++ src/utils/removeMultipleFiles.ts
@@ -1,7 +1,11 @@
 import s3Config from "../config/s3";
 import { DeleteObjectCommand } from "@aws-sdk/client-s3";
+import { TTestFile } from "../types/fileTestType";
+import { TAppointmentFile } from "../types/fileAppointmentType";
 
-export default async function removeMultipleFilesS3(files: any[]) {
+export default async function removeMultipleFilesS3(
+  files: TTestFile[] | TAppointmentFile[]
+) {
   for (let i = 0; i < files.length; i++) {
     const bucketParams = {
       Bucket: process.env.AWS_BUCKET_NAME,
