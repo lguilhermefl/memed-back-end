d979a9057a5c1be9cc0b56a9d00c48844c16ce27
luiz <lguilhermefl@gmail.com>
2022-10-05 13:30:26 -0300

    feat: add find all appointments by user id with files

--- src/repositories/appointmentRepository.ts
+++ src/repositories/appointmentRepository.ts
@@ -12,3 +12,18 @@ export async function findById(id: number) {
     where: { id },
   });
 }
+
+export async function findAllByUserIdWithFiles(userId: number) {
+  return await prisma.$queryRaw`
+    select a.id, a.title, a.notes, a.date, array(
+      select coalesce(
+        json_build_object('id', af.id, 'name', af.name, 'size', af.size, 'url', af.url)
+      , '[] '
+      ) from "appointmentsFiles" af
+      where af."appointmentId"=a.id
+    ) as files
+    from appointments a
+    where a."userId"=${userId}
+    order by a.date
+  `;
+}
