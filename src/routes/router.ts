f90678e62d75534f46bcb8396aa194b6411c0566
luiz <lguilhermefl@gmail.com>
2022-10-05 16:52:09 -0300

    feat: add appointment files router

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -3,11 +3,13 @@ import userRouter from "./userRouter";
 import testRouter from "./testRouter";
 import testFilesRouter from "./testFilesRouter";
 import appointmentRouter from "./appointmentRouter";
+import appointmentFilesRouter from "./appointmentFilesRouter";
 
 const router = Router();
 router.use(userRouter);
 router.use("/tests", testRouter);
 router.use("/tests/files", testFilesRouter);
 router.use("/appointments", appointmentRouter);
+router.use("/appointments/files", appointmentFilesRouter);
 
 export default router;
