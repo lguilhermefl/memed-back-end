3f20a2c6bdeb15a4f1674e3e19160e30b7b40595
luiz <lguilhermefl@gmail.com>
2022-10-05 20:01:23 -0300

    feat: add user schema validation

--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -1,8 +1,11 @@
 import { Router } from "express";
 import * as userController from "../controllers/userController";
+import { userSchema } from "../schemas/userSchema";
+import validateBodySchemaMiddleware from "../middlewares/validateBodySchema";
 
 const userRouter = Router();
 
+userRouter.use(validateBodySchemaMiddleware(userSchema));
 userRouter.post("/sign-up", userController.insert);
 userRouter.post("/", userController.signIn);
 
