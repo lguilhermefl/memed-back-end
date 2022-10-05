325232b8de8384b2f3dddf33ade9204d9e268e0a
luiz <lguilhermefl@gmail.com>
2022-10-05 16:13:32 -0300

    feat: add get all appointments by user id with files

--- src/services/appointment.ts
+++ src/services/appointment.ts
@@ -4,3 +4,7 @@ import * as appointmentRepository from "../repositories/appointmentRepository";
 export async function insert(appointment: TCreateAppointment) {
   return await appointmentRepository.insert(appointment);
 }
+
+export async function getAllByUserIdWithFiles(userId: number) {
+  return await appointmentRepository.findAllByUserIdWithFiles(userId);
+}
