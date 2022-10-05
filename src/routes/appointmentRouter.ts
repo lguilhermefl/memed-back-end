139dcbc8b0bea8307a16aa2543e8d719c3e9e108
luiz <lguilhermefl@gmail.com>
2022-10-05 20:23:52 -0300

    feat: add validate schema appointment router

--- src/routes/appointmentRouter.ts
+++ src/routes/appointmentRouter.ts
@@ -1,11 +1,17 @@
 import { Router } from "express";
 import * as appointmentController from "../controllers/appointmentController";
 import validateJWT from "../middlewares/validateJwtMiddleware";
+import { appointmentSchema } from "../schemas/appointmentSchema";
+import validateBodySchemaMiddleware from "../middlewares/validateBodySchema";
 
 const appointmentRouter = Router();
 
 appointmentRouter.use(validateJWT());
-appointmentRouter.post("/", appointmentController.insert);
+appointmentRouter.post(
+  "/",
+  validateBodySchemaMiddleware(appointmentSchema),
+  appointmentController.insert
+);
 appointmentRouter.get("/", appointmentController.getAll);
 appointmentRouter.delete("/", appointmentController.remove);
 
