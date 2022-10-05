14645d12fe831e91ab6382e248bb443243404cff
luiz <lguilhermefl@gmail.com>
2022-10-05 11:20:40 -0300

    feat: create remove multiple files from aws-s3 function

---
+++ src/utils/removeMultipleFiles.ts
@@ -0,0 +1,14 @@
+import { TTestFile } from "../types/fileTestType";
+import s3Config from "../config/s3";
+import { DeleteObjectCommand } from "@aws-sdk/client-s3";
+
+export default async function removeMultipleFilesS3(files: TTestFile[]) {
+  for (let i = 0; i < files.length; i++) {
+    const bucketParams = {
+      Bucket: process.env.AWS_BUCKET_NAME,
+      Key: files[i].key,
+    };
+
+    await s3Config.send(new DeleteObjectCommand(bucketParams));
+  }
+}
