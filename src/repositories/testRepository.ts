8d0b03b09ce85b671c04bdaaa6597d59957505bb
luiz <lguilhermefl@gmail.com>
2022-10-05 13:37:17 -0300

    refactor: change type of test parameter to TCreateTest

--- src/repositories/testRepository.ts
+++ src/repositories/testRepository.ts
@@ -1,6 +1,7 @@
 import { prisma } from "../config/database";
+import { TCreateTest } from "../types/testType";
 
-export async function insert(test: any) {
+export async function insert(test: TCreateTest) {
   return await prisma.tests.create({
     data: test,
   });
