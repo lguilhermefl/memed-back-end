c8831941479f34f27918ad55328970e3f257a5c9
luiz <lguilhermefl@gmail.com>
2022-10-05 16:51:26 -0300

    feat: add appointment router

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
-import testFilesRouter from "./testFilesRouter";
 import userRouter from "./userRouter";
 import testRouter from "./testRouter";
+import testFilesRouter from "./testFilesRouter";
+import appointmentRouter from "./appointmentRouter";
 
 const router = Router();
 router.use(userRouter);
 router.use("/tests", testRouter);
 router.use("/tests/files", testFilesRouter);
+router.use("/appointments", appointmentRouter);
 
 export default router;
