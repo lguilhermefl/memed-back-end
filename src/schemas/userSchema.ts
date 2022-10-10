07587125183aa0e9f192ff2546618dd275a4a4e6
luiz <lguilhermefl@gmail.com>
2022-10-10 14:19:34 -0300

    feat: add max email length and min password length

--- src/schemas/userSchema.ts
+++ src/schemas/userSchema.ts
@@ -1,6 +1,14 @@
 import joi from "joi";
 
-export const userSchema = joi.object({
-  email: joi.string().email().required(),
+export const signInSchema = joi.object({
+  email: joi.string().max(40).email().required(),
   password: joi.string().min(6).required(),
 });
+
+export const signUpSchema = joi
+  .object({
+    email: joi.string().max(40).email().required(),
+    password: joi.string().min(6).max(40).required(),
+    repeat_password: joi.ref("password"),
+  })
+  .with("password", "repeat_password");
