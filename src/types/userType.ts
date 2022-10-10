dff2bcb30e691ef51c972d0849a50bb554899b83
luiz <lguilhermefl@gmail.com>
2022-10-10 14:22:51 -0300

    refactor: change user types names

--- src/types/userType.ts
+++ src/types/userType.ts
@@ -1,5 +1,8 @@
 import { Users } from "@prisma/client";
 
 export type TUser = Users;
-export type TCreateUser = Omit<TUser, "id" | "createdAt">;
+export type TSignInUser = Omit<TUser, "id" | "createdAt">;
+export interface TCreateUser extends TSignInUser {
+  repeat_password?: string;
+}
 export type TUpdateUser = Partial<TUser>;
